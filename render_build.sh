3fe38d119c19ec29b2b63ff02b5d1d7073e7003a
Osmobaits <radoslawkrawczuk@gmail.com>
2026-01-06 17:50:55 +0100

    G

--- render_build.sh
+++ render_build.sh
@@ -5,8 +5,19 @@ set -o errexit
 pip install -r requirements.txt
 
 # 2. Ręczna korekta wersji migracji w bazie danych Postgres
-# Używamy Pythona, aby załadować aplikację i wykonać zapytanie SQL
-python -c 'from app import create_app; from app.models import db; app = create_app(); with app.app_context(): db.session.execute(db.text("UPDATE alembic_version SET version_num = \"60da692b8fb2\"")); db.session.commit(); print("Wersja migracji została pomyślnie zaktualizowana na 60da692b8fb2")'
+python << END
+from app import create_app
+from app.models import db
+import sqlalchemy
+
+app = create_app()
+with app.app_context():
+    # Używamy text() dla bezpieczeństwa zapytania
+    sql = sqlalchemy.text("UPDATE alembic_version SET version_num = '60da692b8fb2'")
+    db.session.execute(sql)
+    db.session.commit()
+    print("Sukces: Wersja migracji w bazie Postgres zmieniona na 60da692b8fb2")
+END
 
 # 3. Uruchomienie właściwej aktualizacji bazy danych
 flask db upgrade
